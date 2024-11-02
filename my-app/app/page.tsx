4dd5f4ae2b105c48bb027c6070499e33d1526822
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-02 16:46:13 +0000

    project reinstall/reconfig

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section>
+      <p className="md:w-full p-4 text-white">Hello!</p>
+    </section>
+  );
+}
