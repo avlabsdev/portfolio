999d40c6865b1228668f28310d90d2236cc1d6e2
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-02 17:47:13 +0000

    added components, installed packages, css updates

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+
+import { Button } from '@/components/ui/button';
+import ThemeToggle from '@/components/theme-toggle';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+
 // import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      <p className="md:w-full p-4 text-white">Hello!</p>
-    </section>
+    <>
+      <Header />
+      <section className="p-4">
+        <h1>Welcome to my app!</h1>
+        <p>Hello! My name is Aaron and I love to build applications that solve problems for our most pressing issues we face in our world today.</p>
+        <Button>Button</Button>
+        <div className="flex gap-2">
+          <ThemeToggle />
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 }
